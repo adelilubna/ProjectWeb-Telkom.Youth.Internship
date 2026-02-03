5b2210b8c035a02a7a7bab67d9fc02e00eb7bd85
adelilubna <delilubna@gmail.com>
2026-02-03 13:32:10 +0700

    update dashboard admin dan user serta perbaikan footer

--- intern/dashboard/dashboard.js
+++ intern/dashboard/dashboard.js
@@ -1,3 +1,4 @@
+// SIDEBAR
 const toggle = document.getElementById('menuToggle');
 const sidebar = document.querySelector('.sidebar');
 const navItems = document.querySelectorAll('.nav-item');
@@ -11,4 +12,20 @@ navItems.forEach(item => {
     navItems.forEach(n => n.classList.remove('active'));
     item.classList.add('active');
   });
+});
+
+// NOTIF
+const notifBtn = document.getElementById("notifBtn");
+const notifPanel = document.getElementById("notifPanel");
+
+notifBtn.addEventListener("click", function (e) {
+  e.stopPropagation();
+  notifPanel.classList.toggle("active");
+});
+
+// klik di luar = nutup
+document.addEventListener("click", function (e) {
+  if (!e.target.closest(".notif-wrapper")) {
+    notifPanel.classList.remove("active");
+  }
 });
